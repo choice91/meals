3df720c60ce087a9fa73a74cff9eda022192016a
choice.lee <wer1264@gmail.com>
2023-07-23 20:19:19 +0900

    Add: add meal dummy data and screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/categories_screen.dart';
 
-final theme = ThemeData(
+final ThemeData theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
